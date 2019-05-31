a1aa49b63df4115afe61805d7e70bba3b0199c41
randomgamer1017 <40378529+randomgamer1017@users.noreply.github.com>
2019-05-31 10:16:40 -0700

    Add files via upload

--- core/src/Tools/WorldContactListener.java
+++ core/src/Tools/WorldContactListener.java
@@ -13,7 +13,6 @@ public class WorldContactListener implements ContactListener {
 
 	@Override
 	public void beginContact(Contact contact) {
-		Gdx.app.log("Begin Contact", "");
 		Fixture fixA = contact.getFixtureA();
 		Fixture fixB = contact.getFixtureB();
 		
@@ -29,7 +28,7 @@ public class WorldContactListener implements ContactListener {
 
 	@Override
 	public void endContact(Contact contact) {
-		Gdx.app.log("End Contact", "");
+
 	}
 
 	@Override
