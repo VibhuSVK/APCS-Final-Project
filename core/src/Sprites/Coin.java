0f85330f45e7062b66caa3d19444c0310621967b
randomgamer1017 <40378529+randomgamer1017@users.noreply.github.com>
2019-05-31 10:17:26 -0700

    Add files via upload

--- core/src/Sprites/Coin.java
+++ core/src/Sprites/Coin.java
@@ -1,5 +1,6 @@
 package Sprites;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.BodyDef;
@@ -11,5 +12,12 @@ import com.stackoverflowtrio.orbit.Orbit;
 public class Coin extends InteractiveTileObject {
 	public Coin(World world, TiledMap map, Rectangle bounds) {
 		super(world, map, bounds);
+		fixture.setUserData(this);
+		setCategoryFilter(Orbit.COIN_BIT);
+	}
+
+	@Override
+	public void onHeadHit() {
+		Gdx.app.log("Coin", "Collision");
 	}
 }
--- core/src/Sprites/Mario.java
+++ core/src/Sprites/Mario.java
@@ -1,20 +1,105 @@
 package Sprites;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.stackoverflowtrio.orbit.Orbit;
 
+import Screens.PlayScreen;
+
 public class Mario extends Sprite {
+	public enum State {FALLING, JUMPING, STANDING, RUNNING };
+	public State currentState;
+	public State previousState;
 	public World world;
 	public Body b2body;
+	private TextureRegion marioStand;
+	private Animation marioRun;
+	private Animation marioJump;
+	private float stateTimer;
+	private boolean runningRight;
 	
-	public Mario(World world) {
+	public Mario(World world, PlayScreen screen) {
+		super(screen.getAtlas().findRegion("little_mario"));
 		this.world = world;
+		currentState = State.STANDING;
+		previousState = State.STANDING;
+		stateTimer = 0;
+		runningRight = true;
+		
+		Array<TextureRegion> frames = new Array<TextureRegion>();
+		for (int i = 1; i < 4; i++) {
+			frames.add(new TextureRegion(getTexture(), i * 16, 0, 16, 16));
+		}
+		marioRun = new Animation(0.1f, frames);
+		frames.clear();
+		
+		for (int i = 4; i < 6; i++) {
+			frames.add(new TextureRegion(getTexture(), i * 16, 0, 16, 16));
+		}
+		marioJump = new Animation(0.1f, frames);
+		
+		marioStand = new TextureRegion(getTexture(), 0, 0, 16, 16);
+		
 		defineMario();
+		setBounds(0, 0, 16 / Orbit.PPM, 16 / Orbit.PPM);
+		setRegion(marioStand);
+	}
+	
+	public void update(float dt) {
+		setPosition(b2body.getPosition().x - getWidth() / 2, b2body.getPosition().y - getHeight() / 2);
+		setRegion(getFrame(dt));
+	}
+	
+	public TextureRegion getFrame(float dt) {
+		currentState = getState();
+		
+		TextureRegion region;
+		switch(currentState) {
+			case JUMPING:
+				region = (TextureRegion) marioJump.getKeyFrame(stateTimer);
+				break;
+			case RUNNING:
+				region = (TextureRegion) marioRun.getKeyFrame(stateTimer, true);
+				break;
+			case FALLING:
+			case STANDING:
+			default:
+				region = marioStand;
+				break;
+		}
+		
+		if((b2body.getLinearVelocity().x < 0 || !runningRight) && !region.isFlipX()) {
+			region.flip(true, false);
+			runningRight = false;
+		}
+		else if((b2body.getLinearVelocity().x > 0 || runningRight) && region.isFlipX()) {
+			region.flip(true, false);
+			runningRight = true;
+		}
+		
+		stateTimer = currentState == previousState ? stateTimer + dt : 0;
+		previousState = currentState;
+		return region;
+	}
+	
+	public State getState() {
+		if (b2body.getLinearVelocity().y > 0 || b2body.getLinearVelocity().y < 0 && previousState == State.JUMPING)
+			return State.JUMPING;
+		else if (b2body.getLinearVelocity().y < 0)
+			return State.FALLING;
+		else if (b2body.getLinearVelocity().x != 0)
+			return State.RUNNING;
+		else 
+			return State.STANDING;
 	}
 	
 	public void defineMario() {
@@ -25,9 +110,20 @@ public class Mario extends Sprite {
 		
 		FixtureDef fdef = new FixtureDef();
 		CircleShape shape = new CircleShape();
-		shape.setRadius(5 / Orbit.PPM);
+		shape.setRadius(7 / Orbit.PPM);
+		fdef.filter.categoryBits = Orbit.MARIO_BIT;
+		fdef.filter.maskBits = Orbit.DEFAULT_BIT | Orbit.COIN_BIT | Orbit.BRICK_BIT;
 		
 		fdef.shape = shape;
 		b2body.createFixture(fdef);
+		
+		EdgeShape head = new EdgeShape();
+		head.set(new Vector2(-2 / Orbit.PPM, 6  / Orbit.PPM), new Vector2(2 / Orbit.PPM, 6 / Orbit.PPM));
+		fdef.shape = head;
+		fdef.isSensor = true;
+		
+		b2body.createFixture(fdef).setUserData("head");
+		
+		
 	}
 }
