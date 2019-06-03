adea21332c2999f1a7db1d2589e0237c34eb6189
UndeadDragon324 <49879094+UndeadDragon324@users.noreply.github.com>
2019-06-03 10:16:36 -0700

    Add files via upload

--- core/src/com/stackoverflowtrio/orbit/Orbit.java
+++ core/src/com/stackoverflowtrio/orbit/Orbit.java
@@ -9,22 +9,28 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import Screens.MainMenuScreen;
 import Screens.PlayScreen;
 
 public class Orbit extends Game {
 	public static final int V_WIDTH = 400;
 	public static final int V_HEIGHT = 208;
 	public static final float PPM = 100;
-	
 	public SpriteBatch batch;
 	Texture img;
-	
+	private float musicVol = (float) 0.5;
+	private PlayScreen playScreen;
+	private MainMenuScreen menuScreen;
 	@Override
 	public void create () {
+		
 		batch = new SpriteBatch();
-		setScreen(new PlayScreen(this));
-		Music music = Gdx.audio.newMusic(Gdx.files.internal("data/ThemeSong.mp3"));
-		music.play();
+		menuScreen = new MainMenuScreen(this);
+		this.setScreen(menuScreen);
+//		Music music = Gdx.audio.newMusic(Gdx.files.internal("data/AmbientSong.mp3"));
+//		music.setVolume(musicVol);
+//		music.setLooping(true);
+//		music.play();
 	}
 
 	@Override
@@ -37,4 +43,17 @@ public class Orbit extends Game {
 		batch.dispose();
 		img.dispose();
 	}
+	public void changeScreen(int screen){
+	switch(screen){
+		case 0:
+			if(menuScreen == null) menuScreen = new MainMenuScreen(this);
+                        this.setScreen(menuScreen);
+			break;
+		case 1:
+			if(playScreen == null) playScreen = new PlayScreen(this);
+						this.setScreen(playScreen);
+		
+	}
+}
+
 }
